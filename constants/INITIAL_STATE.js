6d54a8e1e8cbe6b666d00dbb2e035a842e319187
Sam Wederell <sam.wederell@gmail.com>
2020-01-24 17:29:17 +0000

    remove extras

--- constants/INITIAL_STATE.js
+++ constants/INITIAL_STATE.js
@@ -1,20 +1,10 @@
-const metaList = require('../assets/data/metalist.json');
-const Rugby = require('../assets/data/rugby-centre.json');
-const Kenilworth = require('../assets/data/kenilworth.json');
-const Braunston = require('../assets/data/braunston.json');
-
-
 const INITIAL_STATE = {
   trailState: {
     showHintModal: false,
-    selectedTrail: '',
+    selectedTrail: "",
     sortIndex: 0,
     metaList,
-    trails: {
-      'rugby-centre': Rugby,
-      kenilworth: Kenilworth,
-      braunston: Braunston
-    },
+    trails: {},
     save: {}
   }
 };
